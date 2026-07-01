0de978bc1b6a4d2572a3cc1e32514610d5512ca3
FebrindaDwiCahyati <febrindadwi14@gmail.com>
2026-07-01 11:18:56 +0700

    update6

--- api/koneksi.php
+++ api/koneksi.php
@@ -5,19 +5,72 @@ $password = "CbZIpuRuZVGl7TME";
 $database = "db_kendaraan_dinas";
 $port     = 4000;
 
-// 1. Inisialisasi objek koneksi MySQLi terlebih dahulu
 $koneksi = mysqli_init();
 
 if (!$koneksi) {
     die("mysqli_init gagal");
 }
 
-// 2. Aktifkan fitur SSL aman (Wajib untuk TiDB Cloud)
-// Nilai parameter dikosongkan karena TiDB mengizinkan handshake otomatis tanpa file CA fisik
 mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
 
-// 3. Lakukan koneksi ke database cloud
 if (!mysqli_real_connect($koneksi, $host, $user, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
+
+mysqli_query($koneksi, "CREATE TABLE IF NOT EXISTS sessions (
+    id VARCHAR(128) PRIMARY KEY,
+    data TEXT,
+    last_access INT
+)");
+
+function session_open_custom($path, $name) {
+    return true;
+}
+
+function session_close_custom() {
+    return true;
+}
+
+function session_read_custom($id) {
+    global $koneksi;
+    $id = mysqli_real_escape_string($koneksi, $id);
+    $hasil = mysqli_query($koneksi, "SELECT data FROM sessions WHERE id = '$id'");
+    $baris = mysqli_fetch_assoc($hasil);
+    if ($baris) {
+        return $baris['data'];
+    }
+    return "";
+}
+
+function session_write_custom($id, $data) {
+    global $koneksi;
+    $id = mysqli_real_escape_string($koneksi, $id);
+    $data = mysqli_real_escape_string($koneksi, $data);
+    $waktu = time();
+    mysqli_query($koneksi, "REPLACE INTO sessions (id, data, last_access) VALUES ('$id', '$data', $waktu)");
+    return true;
+}
+
+function session_destroy_data($id) {
+    global $koneksi;
+    $id = mysqli_real_escape_string($koneksi, $id);
+    mysqli_query($koneksi, "DELETE FROM sessions WHERE id = '$id'");
+    return true;
+}
+
+function session_gc_custom($max_lifetime) {
+    global $koneksi;
+    $batas = time() - $max_lifetime;
+    mysqli_query($koneksi, "DELETE FROM sessions WHERE last_access < $batas");
+    return true;
+}
+
+session_set_save_handler(
+    "session_open",
+    "session_close",
+    "session_read",
+    "session_write",
+    "session_destroy_data",
+    "session_gc"
+);
 ?>
