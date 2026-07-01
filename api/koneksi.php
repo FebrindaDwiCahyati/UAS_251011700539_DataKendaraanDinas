78da69919369b87a1c5eabefa6c3373fd1f806bf
FebrindaDwiCahyati <febrindadwi14@gmail.com>
2026-07-01 10:18:25 +0700

    update3

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,13 +1,23 @@
 <?php
-$host     = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+$host     = "://tidbcloud.com";
 $user     = "3nkBdoh6yKZfSNY.root";
-$password = "CbZIpuRuZVGl7TME"; 
+$password = "CbzIpuhuzVG17IMt"; 
 $database = "db_kendaraan_dinas";
 $port     = 4000;
 
-$koneksi = mysqli_connect($host, $user, $password, $database, $port);
+// 1. Inisialisasi objek koneksi MySQLi terlebih dahulu
+$koneksi = mysqli_init();
 
-if (mysqli_connect_errno()) {
-    echo "Koneksi database gagal: " . mysqli_connect_error();
+if (!$koneksi) {
+    die("mysqli_init gagal");
+}
+
+// 2. Aktifkan fitur SSL aman (Wajib untuk TiDB Cloud)
+// Nilai parameter dikosongkan karena TiDB mengizinkan handshake otomatis tanpa file CA fisik
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+// 3. Lakukan koneksi ke database cloud
+if (!mysqli_real_connect($koneksi, $host, $user, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
 }
 ?>
