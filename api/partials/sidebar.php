a015bf4b4f4e087ece10c60373f31a60a8b066c2
FebrindaDwiCahyati <febrindadwi14@gmail.com>
2026-07-01 12:30:42 +0700

    update13

--- api/partials/sidebar.php
+++ api/partials/sidebar.php
@@ -1,35 +1,36 @@
 <?php
-$halaman = basename($_SERVER['PHP_SELF']);
+// Deteksi URL yang sedang aktif di browser
+$request_uri = $_SERVER['REQUEST_URI'];
 ?>
 
 <div class="sidebar">
     <div class="logo">
-        <img src="assets/image/logo.png" width="70">
+        <img src="../assets/image/logo.png" width="70">
         <h3>Kendaraan Dinas</h3>
         <small>Manajemen Data Kendaraan</small>
     </div>
 
     <ul>
         <li>
-            <a href="home.php" class="<?= ($halaman == 'home.php') ? 'active' : ''; ?>">
+            <a href="home.php" class="<?= (strpos($request_uri, 'home.php') !== false) ? 'active' : ''; ?>">
                 Dashboard
             </a>
         </li>
 
         <li>
-            <a href="tambah.php" class="<?= ($halaman == 'tambah.php') ? 'active' : ''; ?>">
+            <a href="tambah.php" class="<?= (strpos($request_uri, 'tambah.php') !== false) ? 'active' : ''; ?>">
                 Tambah Kendaraan
             </a>
         </li>
 
         <li>
-            <a href="jenis.php" class="<?= ($halaman == 'jenis.php') ? 'active' : ''; ?>">
+            <a href="jenis.php" class="<?= (strpos($request_uri, 'jenis.php') !== false) ? 'active' : ''; ?>">
                 Jenis Kendaraan
             </a>
         </li>
 
         <li>
-            <a href="report.php" class="<?= ($halaman == 'report.php') ? 'active' : ''; ?>">
+            <a href="report.php" class="<?= (strpos($request_uri, 'report.php') !== false) ? 'active' : ''; ?>">
                 Cetak Laporan
             </a>
         </li>
